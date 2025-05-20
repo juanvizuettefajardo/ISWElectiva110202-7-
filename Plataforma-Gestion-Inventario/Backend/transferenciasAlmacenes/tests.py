52e1e84dbaa1a93d8473227560172d5249b4b106
juanvizuettefajardo <juanvizuette58@gmail.com>
2025-05-19 20:47:19 -0500

    third commit

--- Plataforma-Gestion-Inventario/Backend/transferenciasAlmacenes/tests.py
+++ Plataforma-Gestion-Inventario/Backend/transferenciasAlmacenes/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+from rest_framework import serializers
+from .models import Almacen
+
+class AlmacenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Almacen
+        fields = ['id','nombre','ubicacion']
--- Plataforma-Gestion-Inventario/Backend/transferenciasAlmacenes/views.py
+++ Plataforma-Gestion-Inventario/Backend/transferenciasAlmacenes/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
-
+from rest_framework import serializers
+from GestionMovimientos.models import Transferencia
+from GestionProductos.serializers import ProductoSerializer
+from GestionUsuarios.serializers import UsuarioSerializer
+from .serializers import AlmacenSerializer
 # Create your views here.
+
+class TransferenciaSerializer(serializers.ModelSerializer):
+    producto = ProductoSerializer(read_only=True)
+    producto_id = serializers.PrimaryKeyRelatedField(
+        queryset=Transferencia._meta.get_field('producto').related_model.objects.all(),
+        source='producto',
+        write_only = True
+    )
+    
+    origen = AlmacenSerializer(read_only=True)
+    origen_id = serializers.PrimaryKeyRelatedField(
+        queryset=Transferencia._meta.get_field('origen').related_model.objects.all(),
+        source='destino',
+        write_only=True
+    )
+    usuario = UsuarioSerializer(read_only=True)
+    usuario_id = serializers.PrimaryKeyRelatedField(
+        queryset=Transferencia._meta.get_field('usuario').related_model.objects.all(),
+        source='usuario',
+        write_only=True
+    )
+    
+    class Meta:
+        model = Transferencia
+        fields = [
+          'id','producto','producto_id',
+          'cantidad',
+          'origen','origen_id',
+          'destino','destino_id',
+          'usuario','usuario_id',
+          'fecha'
+        ]
+        read_only_fields = ['fecha']
