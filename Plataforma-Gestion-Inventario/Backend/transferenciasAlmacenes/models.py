be7a6137044cc60ae82aedd36707627e82824316
juanvizuettefajardo <juanvizuette58@gmail.com>
2025-05-19 20:11:36 -0500

    first commit

--- Plataforma-Gestion-Inventario/Backend/transferenciasAlmacenes/models.py
+++ Plataforma-Gestion-Inventario/Backend/transferenciasAlmacenes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Almacen(models.Model):
+    nombre = models.CharField(max_length=100)
+    ubicacion = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.nombre
