78802c2ead8554b55a36d91fba5be00e2607317f
juanvizuettefajardo <juanvizuette58@gmail.com>
2025-05-19 21:17:35 -0500

    nine commit

--- Plataforma-Gestion-Inventario/Frontend/src/services/transferenciasService.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const API_URL = import.meta.env.VITE_API_URL;
-
-export const getTransferencias = () => {
-  axios.get(`${API_URL}/transferencias/`).then((res) => res.data);
-};
-
-export const createTransferencia = (data) =>
-  axios.post(`${API_URL}/transferencias/`, data).then((res) => res.data);
